d1110a8d5376ad2ded415ab48d6637521d1b4590
Thiago gutierrez <gutierrezdelatorrej30@gmail.com>
2023-07-31 19:58:25 -0500

    d

--- AlmacenApp/src/ConexionServidor/Conexion.java
+++ AlmacenApp/src/ConexionServidor/Conexion.java
@@ -10,8 +10,8 @@ public class Conexion {
         try{
             String  usu = DBconstacia.usu;
             String cont = DBconstacia.cont;
-            String bd =  DBconstacia.bd;
-            String ip = DBconstacia.ip;
+            String bd =   DBconstacia.bd;
+            String ip =   DBconstacia.ip;
             String puerto  = DBconstacia.puerto;
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String cadena = "jdbc:sqlserver://"+ip+":"+puerto+";"+"databaseName="+bd+";"+"encrypt=true;trustServerCertificate=true";
---
+++ almacen-service/src/test/java/com/excelencia/almacenservice/AlmacenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelencia.almacenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
